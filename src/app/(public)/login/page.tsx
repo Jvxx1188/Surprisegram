a9e274399ff4939f18e26730455056b45afed485
Jvxx1188 <jvxx1188@gmail.com>
2024-03-10 17:45:38 -0300

    changed process env to getserver in register and login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -4,107 +4,108 @@ import "./globals.css";
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from "react-hook-form"
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
+import { serverUrl } from '@/lib/utils';
 dotenv.config()
-const server = process.env.SERVER;
+const server = serverUrl()
 
 const LoginSchema = z.object({
-        email: z.string().email('Preencha corretamente o campo de email'),
-        password: z.string().min(6,'senha de no mínimo 6 caracteres')
-    });
+    email: z.string().email('Preencha corretamente o campo de email'),
+    password: z.string().min(6, 'senha de no mínimo 6 caracteres')
+});
 export default function Login() {
-    const [isSubmiting,setIsSubmiting] = useState(false)
+    const [isSubmiting, setIsSubmiting] = useState(false)
     const router = useRouter()
     //token existe?
-function redirectToMain(){
-    console.log('redirecting')
-    router.push('/')
-}
+    function redirectToMain() {
+        console.log('redirecting')
+        router.push('/')
+    }
 
-useEffect(()=>{
-const token = localStorage.getItem('token')
- if(token) {
-         redirectToMain()
-         console.log('token existe, redirecionando...')
-    } 
-},[])   
- 
-    const [error,setError] = useState('')
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            redirectToMain()
+            console.log('token existe, redirecionando...')
+        }
+    }, [])
+
+    const [error, setError] = useState('')
     const {
-        register,handleSubmit,
+        register, handleSubmit,
         formState: { errors },
-      } = useForm<z.infer<typeof LoginSchema>>(
-        {resolver : zodResolver(LoginSchema)}
-      );
-        
-   
-      
+    } = useForm<z.infer<typeof LoginSchema>>(
+        { resolver: zodResolver(LoginSchema) }
+    );
+
+
+
 
     return <>
-    <div className="flex flex-col gap-4">
-    <h1 className="text-3xl font-bold">Login</h1>
-    
-    <form className="flex flex-col gap-4 justify-center " onSubmit={handleSubmit((data)=>PostForm(data,redirectToMain,isSubmiting,setIsSubmiting))}>
-        {/*EMAIL*/}
         <div className="flex flex-col gap-4">
-            <label className="text-gray-500 " htmlFor="email">Enter your email</label>
-            <input {...register("email")} className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="email" id="email" name="email"/>
+            <h1 className="text-3xl font-bold">Login</h1>
+
+            <form className="flex flex-col gap-4 justify-center " onSubmit={handleSubmit((data) => PostForm(data, redirectToMain, isSubmiting, setIsSubmiting))}>
+                {/*EMAIL*/}
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500 " htmlFor="email">Enter your email</label>
+                    <input {...register("email")} className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="email" id="email" name="email" />
+                </div>
+                {errors.email && <p>{errors.email.message}</p>}
+                {/*PASSWORD*/}
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500" htmlFor="password">Enter your password</label>
+                    <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="password" {...register("password")} />
+                </div>
+                {errors.password && <p>{errors.password.message}</p>}
+                <button type="submit" className="bg-green-400 p-3 rounded-xl flex self-center justify-center duration-200 hover:bg-black hover:text-white">
+                    {isSubmiting ? 'Entrando...' : 'Login'}
+                </button>
+
+            </form>
+
+            <span className="text-gray-800 ">{`Don't have an account?`} <a className="text-cyan-500 hover:underline" href="/register">Sign Up</a></span>
+            <p className='text-red-500 text-end' id="login-return-error"></p>
         </div>
-       {errors.email && <p>{errors.email.message}</p>}
-        {/*PASSWORD*/}
-        <div className="flex flex-col gap-4">
-            <label className="text-gray-500" htmlFor="password">Enter your password</label>
-            <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="password" {...register("password")}/>
-        </div>
-        {errors.password && <p>{errors.password.message}</p>}
-        <button type="submit" className="bg-green-400 p-3 rounded-xl flex self-center justify-center duration-200 hover:bg-black hover:text-white">
-        {isSubmiting ? 'Entrando...' : 'Login'}
-        </button>
-      
-        </form>
-        
-        <span className="text-gray-800 ">{`Don't have an account?`} <a className="text-cyan-500 hover:underline" href="/register">Sign Up</a></span>
-      <p className='text-red-500 text-end' id="login-return-error"></p>
-    </div>
     </>
 }
 
 
-function PostForm(data : any,redirect :()=> void,isSubmiting : boolean,SetisSubmiting : (bol : boolean) => void){
-    if(isSubmiting) return console.log('ja submitando');
+function PostForm(data: any, redirect: () => void, isSubmiting: boolean, SetisSubmiting: (bol: boolean) => void) {
+    if (isSubmiting) return console.log('ja submitando');
     SetisSubmiting(true)
-    console.log(data) 
-     
-     axios.post(`${server}/auth/login`,data).then((res)=> {
-        
-         //reset error tag
-         const errorTag = document.getElementById('login-return-error')
-         if(!errorTag) return console.log('nulo', errorTag)
-         errorTag.innerHTML = ''
+    console.log(data)
 
-         //return window.location.replace("/register");
-         const {token} =JSON.parse(res.request.response);
-         console.log(typeof token)
-         localStorage.setItem('token',token)
-         console.log(token)
-         return redirect()
-     }).catch(async (err)=> {
+    axios.post(`${server}/auth/login`, data).then((res) => {
+
+        //reset error tag
+        const errorTag = document.getElementById('login-return-error')
+        if (!errorTag) return console.log('nulo', errorTag)
+        errorTag.innerHTML = ''
+
+        //return window.location.replace("/register");
+        const { token } = JSON.parse(res.request.response);
+        console.log(typeof token)
+        localStorage.setItem('token', token)
+        console.log(token)
+        return redirect()
+    }).catch(async (err) => {
         SetisSubmiting(false)
         const errorTag = document.getElementById('login-return-error')
-         if(!errorTag) return 
+        if (!errorTag) return
         //se o servidor estiver offline
-        if(!err.request.response) return toast.error('Servidor possívelmente offline');
+        if (!err.request.response) return toast.error('Servidor possívelmente offline');
         //se realmente deu erro
-         const error =await JSON.parse(err.request.response).error
-         
-        return toast.error(error);
-     })
+        const error = await JSON.parse(err.request.response).error
 
- //chamar a api na rota publica(onde nao tem jwt e todo mundo pode)
- 
- }
+        return toast.error(error);
+    })
+
+    //chamar a api na rota publica(onde nao tem jwt e todo mundo pode)
+
+}
 //BY JVXX
--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -4,124 +4,125 @@ import "./globals.css";
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from "react-hook-form"
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { json } from "stream/consumers";
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
-import {toast} from 'sonner'
+import { toast } from 'sonner'
 import Link from 'next/link'
+import { serverUrl } from '@/lib/utils';
 dotenv.config()
-const server = process.env.SERVER;
+const server = serverUrl()
 
 const RegisterSchema = z.object({
-        username : z.string().min(4,'nome de no mínimo 4 caracteres'),
-        email: z.string().email('Preencha corretamente o campo de email'),
-        password: z.string().min(6,'senha de no mínimo 6 caracteres'),
-        confirmPassword: z.string(),
-    }).refine(
-        (value)=>{
-            return value.password === value.confirmPassword;
-        }
+    username: z.string().min(4, 'nome de no mínimo 4 caracteres'),
+    email: z.string().email('Preencha corretamente o campo de email'),
+    password: z.string().min(6, 'senha de no mínimo 6 caracteres'),
+    confirmPassword: z.string(),
+}).refine(
+    (value) => {
+        return value.password === value.confirmPassword;
+    }
     ,
     {
         message: "Confirmação de senha é diferente da senha",
         path: ["confirmPassword"]
     }
-    );
+);
 
 export default function Register() {
-     const router = useRouter()
-     const [isSubmiting,setIsSubmiting] = useState(false)
-useEffect(()=>{
-const token = localStorage.getItem('token')
- if(token) {
-         redirectToMain()
-         console.log('token existe, redirecionando...')
-    } 
-},[])   
- 
-function redirectToMain(){
-    console.log('redirecting')
-    router.push('/login')
-}
+    const router = useRouter()
+    const [isSubmiting, setIsSubmiting] = useState(false)
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            redirectToMain()
+            console.log('token existe, redirecionando...')
+        }
+    }, [])
+
+    function redirectToMain() {
+        console.log('redirecting')
+        router.push('/login')
+    }
 
 
     const {
-        register,handleSubmit,
+        register, handleSubmit,
         formState: { errors },
-      } = useForm<z.infer<typeof RegisterSchema>>(
-        {resolver : zodResolver(RegisterSchema)}
-      );
+    } = useForm<z.infer<typeof RegisterSchema>>(
+        { resolver: zodResolver(RegisterSchema) }
+    );
 
     return <>
-    <div className="flex flex-col gap-6 ">
-    <h1 className="text-3xl font-bold">Register</h1>
-    
-    <form className="flex flex-col gap-4 justify-center " onSubmit={handleSubmit((data)=>PostFormRegister(data,redirectToMain,isSubmiting,setIsSubmiting))}>
-        
-        {/*Username*/}
-        <div className="flex flex-col gap-4">
-            <label className="text-gray-500 " htmlFor="username">Enter your username</label>
-            <input  className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="text" id="username" {...register("username")}/>
-        </div>
-        {errors.username && <p>errors.username.message</p>}
-        {/*EMAIL*/}
-        <div className="flex flex-col gap-4">
-            <label className="text-gray-500 " htmlFor="email">Enter your email</label>
-            <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="email" id="email"  {...register("email")}/>
-        </div>
-       {errors.email && <p>errors.email.message</p>}
+        <div className="flex flex-col gap-6 ">
+            <h1 className="text-3xl font-bold">Register</h1>
 
-        {/*PASSWORD*/}
-        
-        <div className="flex flex-col gap-4">
-            <label className="text-gray-500" htmlFor="password">Enter your password</label>
-            <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="confirmpassword" {...register("password")}/>
-        </div>
-        {/*COMFIRM PASSWORD */}
-        <div className="flex flex-col gap-4">
-            <label className="text-gray-500" htmlFor="password">Confirm password</label>
-            <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="confirmpassword" {...register("confirmPassword")}/>
-        </div>
-        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-        
-        {errors.password && <p>{errors.password.message}</p>}
+            <form className="flex flex-col gap-4 justify-center " onSubmit={handleSubmit((data) => PostFormRegister(data, redirectToMain, isSubmiting, setIsSubmiting))}>
 
-        <button type="submit" className="bg-green-400 p-3 rounded-xl flex self-center justify-center duration-200 hover:bg-black hover:text-white">
-           {isSubmiting ? 'Registrando' : 'Registrar'}
-            </button>
-      
-     
-        </form>
-        <span className="text-gray-800">Already have an account? <Link className="text-cyan-500 text-underline hover:underline" href="/login">Sing in</Link></span>
-        <p className='text-red-500 text-end' id="register-return-error"></p>
-    </div>
+                {/*Username*/}
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500 " htmlFor="username">Enter your username</label>
+                    <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="text" id="username" {...register("username")} />
+                </div>
+                {errors.username && <p>errors.username.message</p>}
+                {/*EMAIL*/}
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500 " htmlFor="email">Enter your email</label>
+                    <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="email" id="email"  {...register("email")} />
+                </div>
+                {errors.email && <p>errors.email.message</p>}
+
+                {/*PASSWORD*/}
+
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500" htmlFor="password">Enter your password</label>
+                    <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="confirmpassword" {...register("password")} />
+                </div>
+                {/*COMFIRM PASSWORD */}
+                <div className="flex flex-col gap-4">
+                    <label className="text-gray-500" htmlFor="password">Confirm password</label>
+                    <input className=" outline-none border-b-2 transition duration-300 focus:border-gray-700" type="password" id="confirmpassword" {...register("confirmPassword")} />
+                </div>
+                {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+
+                {errors.password && <p>{errors.password.message}</p>}
+
+                <button type="submit" className="bg-green-400 p-3 rounded-xl flex self-center justify-center duration-200 hover:bg-black hover:text-white">
+                    {isSubmiting ? 'Registrando' : 'Registrar'}
+                </button>
+
+
+            </form>
+            <span className="text-gray-800">Already have an account? <Link className="text-cyan-500 text-underline hover:underline" href="/login">Sing in</Link></span>
+            <p className='text-red-500 text-end' id="register-return-error"></p>
+        </div>
     </>
 }
 
-function PostFormRegister(data : any,redirect : ()=> void,isSubmiting : boolean,SetisSubmiting : (bol : boolean) => void){
-    if(isSubmiting) return console.log('ja submitando');
+function PostFormRegister(data: any, redirect: () => void, isSubmiting: boolean, SetisSubmiting: (bol: boolean) => void) {
+    if (isSubmiting) return console.log('ja submitando');
     SetisSubmiting(true)
     const errorTag = document.getElementById('register-return-error')
-    if(!errorTag) return console.log('nulo', errorTag)
-    
-    
+    if (!errorTag) return console.log('nulo', errorTag)
+
+
     console.log(data)
-    
-    axios.post(`${server}/auth/register`,data).then((res)=> {
+
+    axios.post(`${server}/auth/register`, data).then((res) => {
         errorTag.innerHTML = ''
         toast.success('Sucesso, redirecionando...')
         redirect()
-    }).catch(async (err)=> {
-       //se o servidor estiver offline
-        if(!err.request.response) return errorTag.innerHTML = 'servidor possívelmente offline';
+    }).catch(async (err) => {
+        //se o servidor estiver offline
+        if (!err.request.response) return errorTag.innerHTML = 'servidor possívelmente offline';
         //erros de server
         const error = await JSON.parse(err.request.response).error
-        
+
         SetisSubmiting(false)
-         return toast.error(error);
-        
+        return toast.error(error);
+
     })
-//chamar a api na rota publica(onde nao tem jwt e todo mundo pode)
+    //chamar a api na rota publica(onde nao tem jwt e todo mundo pode)
 
 }
